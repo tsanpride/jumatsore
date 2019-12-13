c44a22dc850189daa418ea648848202f206fcd41
tsantsuy <mail.tsany@gmail.com>
2019-12-13 16:17:24 +0700

    kode asli 100%

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'dbhore');
+ 
+/* Attempt to connect to MySQL database with oop */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+?>
